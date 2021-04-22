8ab0ec4b54f38e4f73417a328101d3520a51d23b
Guito14 <59618151+Guito14@users.noreply.github.com>
2021-04-22 16:09:52 +0100

    2nd Attack +-

--- Assets/Boss_Run.cs
+++ Assets/Boss_Run.cs
@@ -25,7 +25,11 @@ public class Boss_Run : StateMachineBehaviour
         if (distance <= attackRange)
         {
             agent.isStopped = true;
-            animator.SetTrigger("Attack");
+            int rand = Random.Range(1, 4);
+            if (rand == 1)
+                animator.SetTrigger("Attack");
+            else
+               animator.SetTrigger("Attack2");
         }
         agent.isStopped = false;
         if (boss.currentHealth <= 0)
@@ -36,5 +40,6 @@ public class Boss_Run : StateMachineBehaviour
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("Attack");
+        animator.ResetTrigger("Attack2");
     }
 }
