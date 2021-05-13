a7383b22dc109563914118caeee401343675e32f
GitSooth <72495701+GitSooth@users.noreply.github.com>
2021-05-13 22:10:35 +0100

    Fall damage (inicial)

---
+++ Assets/Scripts/Player/FallDamage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    public Vector3 enterPos;
+    public Vector3 exitPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ground")
+        {
+            enterPos = transform.position;
+
+            if(exitPos.y - enterPos.y > 0.2f)
+            {
+                this.GetComponent<PlayerController>().currentHealth -= 5 * exitPos.y - enterPos.y;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            exitPos = transform.position;
+        }
+    }
+}
