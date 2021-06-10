6a54a8e59586bab16d519ad6684a0d473a289c2d
Guito14 <59618151+Guito14@users.noreply.github.com>
2021-06-10 17:45:15 +0100

    Lvl0Holo

---
+++ Assets/quest0.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quest0 : MonoBehaviour
+{
+    private DialogueSystem dialogueSystem;
+
+    //Nome do NPC
+    public string Name;
+
+    //Mexer no tamanho da Area da Caixa de Texto, danos mais espaço para escrever nas frases
+    [TextArea(5, 10)]
+
+    public string[] phrases;
+
+
+    void Start()
+    {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<SpawnPlayer>().positioned)
+        {
+            dialogueSystem.DropDialogue();
+        }
+
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        this.gameObject.GetComponent<Quests2>().enabled = true;
+        FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                dialogueSystem.Names = Name;
+                dialogueSystem.dialogueLines = phrases;
+                FindObjectOfType<DialogueSystem>().NPCName();
+            }
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        // Hud_StartLvL1.SetActive(false);
+        FindObjectOfType<DialogueSystem>().OutOfRange();
+        this.gameObject.GetComponent<Quests2>().enabled = false;
+    }
+
+
+}
